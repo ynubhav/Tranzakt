b333f007e24ed9dc547434a975d1bc7be27e6f2d
ynubhav <anubhavdixit35@gmail.com>
2025-07-10 01:01:09 +0530

    Added Debouncing

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -60,7 +60,7 @@ export default function Dashboard(){
     )
 }
 
-//=============useDebounce==============//
+//=========== ==useDebounce==============//
 function useDebounce(value){
 
     const [dbdvalue,setdbdvalue]=useState(value);
