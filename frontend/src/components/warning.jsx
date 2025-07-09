c415cfb9811e4bec3f8dd714f32b7d43b760be44
ynubhav <anubhavdixit35@gmail.com>
2025-07-10 01:00:43 +0530

    Initial commit

--- frontend/src/components/warning.jsx
+++ frontend/src/components/warning.jsx
@@ -4,7 +4,7 @@ export default function Warning({description,topage,onClick}){
     
     return(
         <div className="mx-5 mt-2 mb-4 flex"> 
-        <div className="font-medium">{description}{'? '}</div>
+        <div className="font-medium ">{description}{'? '}</div>
         <button onClick={onClick} className="font-medium underline hover:cursor-pointer pl-0.5">{topage}</button>
         </div>
     )
