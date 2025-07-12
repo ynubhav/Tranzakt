92b64f1f6c921dc3a0ed52c287915b70854651c4
ynubhav <anubhavdixit35@gmail.com>
2025-07-13 01:11:15 +0530

    changes in style

--- frontend/src/components/profbutton.jsx
+++ frontend/src/components/profbutton.jsx
@@ -1,7 +1,7 @@
 export default function ProfButton({name,onClick}){
     return(
         <div className='grid '>
-            <button onClick={onClick} className="bg-blue-500 rounded-xl py-2 hover:bg-gray-700 text-white text-center mx-5 mb-4 mt-2 cursor-pointer">
+            <button onClick={onClick} className= "rounded-xl py-2 pl-4 hover:bg-gray-700 text-white text-left mx-5 mb-4 mt-2 cursor-pointer">
             {name}
         </button>
         </div>
