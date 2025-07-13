4e492d8e26e7b36c599bdc11156b8bcf6cb71164
ynubhav <anubhavdixit35@gmail.com>
2025-07-14 00:15:47 +0530

    added transaction history and friends

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,32 @@
 const mongoose=require('mongoose')
-const {connectionstring}=require('./config')
+const {connectionstring}=require('./config');
+const { Schema, boolean } = require('zod');
 
 mongoose.connect('mongodb+srv://anubhavdixit35:znzf2GhH8dwISgi5@cluster0.zrb06vd.mongodb.net/Paytmapp');
 //please don't !!! 
-const Userschema= new mongoose.Schema({username:{type:String,unique:true}, firstname: String, lastname:String , password:String });
+const transactionschema=new mongoose.Schema({
+    userId:{type:mongoose.Schema.Types.ObjectId},
+    firstname:String,
+    credit:Boolean,
+    amount:Number,
+    time: { type: Date }
+})
 
-const accountschema=new mongoose.Schema({userId:{type : mongoose.Schema.Types.ObjectId,ref:"User"},balance:Number});
+const Userschema= new mongoose.Schema(
+    {
+        username:{type:String,unique:true},
+        firstname: String,
+        lastname:String,
+        password:String,
+        friends:[{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+    });
+
+const accountschema=new mongoose.Schema(
+    {
+        userId:{type : mongoose.Schema.Types.ObjectId,ref:"User"},
+        balance:Number,
+        transctions:[transactionschema],
+    });
 
 const Accounts=mongoose.model('Accounts',accountschema);
 const User = mongoose.model('User',Userschema);
@@ -14,3 +35,5 @@ module.exports={
     User,
     Accounts
 }
+
+// for every user   User:friends array={userid}, Account:tranzaktions array=[{credit/debit , userid , time/date, amount }],
