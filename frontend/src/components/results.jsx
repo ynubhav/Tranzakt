54ca89daa6fa5c370fdcd76c8575ec8fda435c2f
ynubhav <anubhavdixit35@gmail.com>
2025-07-14 21:29:02 +0530

    adjusted acc to req of friends feature

--- frontend/src/components/results.jsx
+++ frontend/src/components/results.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import More from "./moreinfo";
 
-export default function Searchedusers({firstname,lastname,username,onClick,onMore}){
+export default function Searchedusers({firstname,lastname,username,button1,onClick1,onClick2,onMore}){
     const [rendermore,setmore]=useState(false);
 return(<><div className="mx-2">
         <span onClick={()=>{setmore(!rendermore)}} className="w-1/1 p-2 border-2 border-gray-300 flex justify-between my-2 rounded-2xl bg-blue-500">
@@ -10,7 +10,10 @@ return(<><div className="mx-2">
                 <div className="font-bold text-xl text-gray-900 text-center m-2 hover:cursor-pointer">{firstname}</div>
         </div>
         {rendermore&&<More username={username} lastname={lastname}/>}
-            <div ><button onClick={onClick} className="bg-black  text-white font-medium py-2 px-4 rounded-md hover:bg-gray-500 hover:cursor-pointer">Send Money</button></div>
+            <div className="flex gap-1">
+                {<button onClick={onClick2} className="bg-black  text-white font-medium py-2 px-4 rounded-md hover:bg-gray-500 hover:cursor-pointer">{button1} friend</button>}
+                <button onClick={onClick1} className="bg-black  text-white font-medium py-2 px-4 rounded-md hover:bg-gray-500 hover:cursor-pointer">Send Money</button>
+            </div>
         </span>
         </div>
 </>
