5282c875338602e040efb99e1f2e8c27407cfe7c
ynubhav <anubhavdixit35@gmail.com>
2025-07-16 01:16:59 +0530

    .

--- frontend/src/components/profbutton.jsx
+++ frontend/src/components/profbutton.jsx
@@ -1,7 +1,7 @@
 export default function ProfButton({name,onClick}){
     return(
-        <div className='grid '>
-            <button onClick={onClick} className= "rounded-xl py-2 pl-4 hover:bg-gray-700 text-white text-left mx-5 mb-4 mt-2 cursor-pointer">
+        <div className='grid grid-cols-1'>
+            <button onClick={onClick} className= "rounded-xl py-2 pl-4 hover:bg-gray-700 hover:transition text-white text-left mx-5 mb-4 mt-2 cursor-pointer">
             {name}
         </button>
         </div>
