88845d9a4a067272d833c0e4496f43100ca2eacb
ynubhav <anubhavdixit35@gmail.com>
2025-07-16 01:16:41 +0530

    better readability

--- frontend/src/components/fields.jsx
+++ frontend/src/components/fields.jsx
@@ -3,7 +3,7 @@ export default function Field({fname,fplaceholder,ftype,onChange}){
     return(
         <div className="mx-5 my-2">
         <div className="py-1 font-medium">{fname}</div>
-        <input onChange={onChange} type={ftype} placeholder={fplaceholder} className="text-gray-500 text-left border-1 border-gray-500 rounded-xl py-2 px-4 w-1/1"/>
+        <input onChange={onChange} type={ftype} placeholder={fplaceholder} className="text-gray-500 text-left border-1 border-gray-500 rounded-xl py-2 px-4 w-1/1 focus:text-black"/>
         </div>
     )
 }
