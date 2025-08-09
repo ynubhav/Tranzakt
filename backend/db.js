b413a700835ed649280d531e6baec196566a0ee9
ynubhav <anubhavdixit35@gmail.com>
2025-08-09 20:06:49 +0530

    Refactor database connection to use environment variables and improve connection handling

--- backend/db.js
+++ backend/db.js
@@ -1,39 +1,46 @@
-const mongoose=require('mongoose')
-const {connectionstring}=require('./config');
-const { Schema, boolean } = require('zod');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-mongoose.connect('mongodb+srv://anubhavdixit35:znzf2GhH8dwISgi5@cluster0.zrb06vd.mongodb.net/Paytmapp');
-//please don't !!! 
-const transactionschema=new mongoose.Schema({
-    userId:{type:mongoose.Schema.Types.ObjectId},
-    firstname:String,
-    credit:Boolean,
-    amount:Number,
-    time: { type: Date }
-})
+const connectdb = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_STR);
+    console.log("connect to database");
+  } catch (err) {
+    console.log("could not establish a connection");
+  }
+};
 
-const Userschema= new mongoose.Schema(
-    {
-        username:{type:String,unique:true},
-        firstname: String,
-        lastname:String,
-        password:String,
-        friends:[{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
-    });
+connectdb();
 
-const accountschema=new mongoose.Schema(
-    {
-        userId:{type : mongoose.Schema.Types.ObjectId,ref:"User"},
-        balance:Number,
-        transctions:[transactionschema],
-    });
+const transactionschema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId },
+  firstname: String,
+  credit: Boolean,
+  amount: Number,
+  time: { type: Date },
+});
 
-const Accounts=mongoose.model('Accounts',accountschema);
-const User = mongoose.model('User',Userschema);
+const Userschema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  firstname: String,
+  lastname: String,
+  password: String,
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+});
 
-module.exports={
-    User,
-    Accounts
-}
+const accountschema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  balance: Number,
+  transctions: [transactionschema],
+});
+
+const Accounts = mongoose.model("Accounts", accountschema);
+const User = mongoose.model("User", Userschema);
+
+module.exports = {
+  User,
+  Accounts,
+};
 
 // for every user   User:friends array={userid}, Account:tranzaktions array=[{credit/debit , userid , time/date, amount }],
