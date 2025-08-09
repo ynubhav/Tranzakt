292c6dedafdf137be9962fae5c44033c569c2040
ynubhav <anubhavdixit35@gmail.com>
2025-08-09 20:06:15 +0530

    Remove sensitive configuration file

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-
-const JWT_SECRET='1234556';
-
-const connectionstring='mongodb+srv://anubhavdixit35:znzf2GhH8dwISgi5@cluster0.zrb06vd.mongodb.net/Paytmapp';
-
-module.exports={JWT_SECRET,connectionstring};
