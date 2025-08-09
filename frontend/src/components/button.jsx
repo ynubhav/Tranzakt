e764920bb444dacd24dd727e3081af3b64a7ac19
ynubhav <anubhavdixit35@gmail.com>
2025-08-09 20:11:52 +0530

    Add type attribute to button for improved form submission handling

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -1,6 +1,6 @@
 export default function Button({name,onClick}){
     return(
-        <button onClick={onClick} className="bg-orange-500 rounded-xl py-2 hover:bg-gray-700 text-white text-center mx-5 mb-4 my-2 cursor-pointer">
+        <button type="submit" onClick={onClick} className="bg-orange-500 rounded-xl py-2 hover:bg-gray-700 text-white text-center mx-5 mb-4 my-2 cursor-pointer">
             {name}
         </button>
     )
